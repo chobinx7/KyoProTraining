fc56af782443579df12497099c3e7dddbc1df6ed
takahiro matsuda <chobinx7@gmail.com>
2026-03-01 11:50:11 +0900

    ABC_b 文字数カウント WA

---
+++ ABC447/B.py
@@ -0,0 +1,16 @@
+S = input()
+# for i in range(len(S)):
+# print(S[i])
+char = max(S)
+S.pop(char)
+print(S)
+# ここまでで分からなくなった
+
+# Claudeに作成してもらったコード
+from collections import Counter
+
+S = input()
+cnt = Counter(S)
+max_count = max(cnt.values())
+remove = {c for c, n in cnt.items() if n == max_count}
+print("".join(c for c in S if c not in remove))
